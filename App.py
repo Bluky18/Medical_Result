c609ced937e90dc8d1fb3a9cd738fa996dc037cc
Bluky18 <harshitcsb@gmail.com>
2026-01-17 21:29:45 +0530

    Update App.py

--- App.py
+++ App.py
@@ -25,7 +25,7 @@ Heart_rate = st.number_input("Patient's Heart rate",value=None)
 Systolic_blood_pressure = st.number_input("Patient's Systolic blood pressure",value=None)
 Diastolic_blood_pressure = st.number_input("Patient's Diastolic blood pressure",value=None)
 Blood_sugar = st.number_input("Patient's Blood sugar",value=None)
-CK_MB = st.number_input("Patient's CK-MB",value=None)
+Ck_MB = st.number_input("Patient's CK-MB",value=None)
 
 
 
@@ -38,7 +38,7 @@ if st.button('Predict'):
                           Systolic_blood_pressure,
                           Diastolic_blood_pressure,
                           Blood_sugar,
-                          CK-MB,
+                          Ck_MB,
                           Troponin
                         ]])
   prediction = model.predict(input_data) [0]
